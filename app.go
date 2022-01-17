961d7f15e171a1620023cdc42d2a60bbb4511375
ryosukeYamazaki <ryamazaki@c-fo.com>
2022-01-17 11:57:37 +0900

    :+1: PathからPostIDが得られるようにする

--- app.go
+++ app.go
@@ -11,8 +11,10 @@ import (
 )
 
 func main() {
-	permalink := "https://freee.workplace.com/groups/1779121192119476/permalink/1779125452119050/"
-	postId := PostID(permalink)
+	// permalink := "https://freee.workplace.com/groups/1779121192119476/permalink/3594996737198570/"
+	workplacePath := "/groups/1779121192119476/permalink/3594996737198570/"
+	// postId := PostID(permalink)
+	postIdFromPath := PostIDFrom(workplacePath)
 
 	access_token := os.Getenv("ACCESS_TOKEN")
 	app_id := os.Getenv("APP_ID")
@@ -20,8 +22,14 @@ func main() {
 	globalApp := fb.New(app_id, app_secrete)
 	globalApp.EnableAppsecretProof = true
 	session := globalApp.Session(access_token)
-	res, _ := session.Get(postId, fb.Params{"fields": "message"})
+	res, _ := session.Get(postIdFromPath, fb.Params{"fields": "message"})
 	fmt.Println("Here is my Facebook first name:", res["message"])
+}
+
+func PostIDFrom(workplacePath string) string {
+	splitedPath := strings.Split(workplacePath, "/")
+	// TODO error 処理
+	return splitedPath[4]
 }
 
 func PostID(workplaceURL string) string {
